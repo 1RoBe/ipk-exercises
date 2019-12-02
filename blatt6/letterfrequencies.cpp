577dbabfdb0ec07dd3981c66d3d8438f58f73712
1RoBe <58035534+1RoBe@users.noreply.github.com>
2019-12-02 23:51:02 +0100

    nicht ganz verständlich wie cin hier funktioniert

--- blatt6/letterfrequencies.cpp
+++ blatt6/letterfrequencies.cpp
@@ -5,10 +5,11 @@
 #include <map>
 
 std::map<char,int> get_frequencies();
-void print_frequencies(const std::map<char,int>& frequencies);
+//void print_frequencies(const std::map<char,int>& frequencies);
 
 int main(int argc, char** argv) {
   get_frequencies();
+  //print_frequencies(const std::map<char,int>& frequencies);
   return 0;
 }
 
@@ -19,17 +20,19 @@ std::map<char,int> get_frequencies() {
     // read in character
     std::cin >> c;
     // abort if input closed
-    if (not std::cin) {
+    if (not std::cin)
       break;
-    }
+
     lettList[c]++;
   }
+  std::cout << "it worked" << '\n';
   return get_frequencies();
 }
-
-voidprint_frequencies(conststd::map<char,int>& frequencies) {
+/*
+void print_frequencies(const std::map<char,int>& frequencies) {
    map<char , int >::iterator itr;
    for(itr=m.begin();itr!=m.end();itr++)
 
     cout<<itr->first<<" - "<<itr->second<<endl;
 }
+*/
